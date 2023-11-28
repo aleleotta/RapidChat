848e462300759c24c9e575d551355e41259ba00c
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-28 11:02:55 +0100

    .

--- app/src/main/java/com/example/rapidchat/entities/Message.java
+++ app/src/main/java/com/example/rapidchat/entities/Message.java
@@ -12,12 +12,62 @@ public class Message {
     private String mensaje = "";
 
     public Message(int id, int numSender, int numReceiver, int statusNum, Timestamp timestamp, String mensaje) {
-        this.id = id;
-        this.numSender = numSender;
-        this.numReceiver = numReceiver;
-        this.statusNum = statusNum;
-        this.timestamp = timestamp;
-        this.mensaje = mensaje;
+        if (id > 0) {
+            this.id = id;
+        }
+        String strNumSender = numSender + "";
+        if (strNumSender.length() == 9) {
+            this.numSender = numSender;
+        }
+        String strNumReceiver = numReceiver + "";
+        if (strNumReceiver.length() == 9) {
+            this.numReceiver = numReceiver;
+        }
+        if (statusNum == 1 || statusNum == 2 || statusNum == 3) {
+            this.statusNum = statusNum;
+        }
+        if (timestamp != null && timestamp.getTime() != 0) {
+            this.timestamp = timestamp;
+        }
+        if (!mensaje.equals("") && mensaje != null) {
+            this.mensaje = mensaje;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNumSender() {
+        return numSender;
+    }
+
+    public int getNumReceiver() {
+        return numReceiver;
+    }
+
+    public int getStatusNum() {
+        return statusNum;
+    }
+
+    public void setStatusNum(int statusNum) {
+        if (statusNum == 1 || statusNum == 2 || statusNum == 3) {
+            this.statusNum = statusNum;
+        }
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        if (!mensaje.equals("") && mensaje != null) {
+            this.mensaje = mensaje;
+        }
     }
 
 }
