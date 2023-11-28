1197979e7ecaf489b428a585b06b800c561adb38
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-28 12:48:01 +0100

    .

--- app/src/main/java/com/example/rapidchat/ContactList.kt
+++ app/src/main/java/com/example/rapidchat/ContactList.kt
@@ -2,6 +2,11 @@ package com.example.rapidchat
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rapidchat.ui.theme.RapidChatTheme
+
+@Composable
+fun ContactView(modifier: Modifier = Modifier) {}
 
 @Composable
 fun TopBar(modifier: Modifier = Modifier) {}
@@ -11,3 +16,27 @@ fun List(modifier: Modifier = Modifier) {}
 
 @Composable
 fun ContactItem(modifier: Modifier = Modifier) {}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    RapidChatTheme {
+        TopBar()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListPreview() {
+    RapidChatTheme {
+        List()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContactItemPreview() {
+    RapidChatTheme {
+        ContactItem()
+    }
+}
--- app/src/main/java/com/example/rapidchat/Login.kt
+++ app/src/main/java/com/example/rapidchat/Login.kt
@@ -2,9 +2,27 @@ package com.example.rapidchat
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rapidchat.ui.theme.RapidChatTheme
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {}
 
 @Composable
 fun RegisterScreen(modifier: Modifier = Modifier) {}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    RapidChatTheme {
+        LoginScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterScreenPreview() {
+    RapidChatTheme {
+        RegisterScreen()
+    }
+}
