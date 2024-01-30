970773e79062408721d8fbecdca6fb2e324495de
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-01-30 08:25:42 +0100

    .

--- app/src/main/java/com/example/rapidchat/MainActivity.kt
+++ app/src/main/java/com/example/rapidchat/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.compose.rememberNavController
 import com.example.rapidchat.ui.theme.RapidChatTheme
 
